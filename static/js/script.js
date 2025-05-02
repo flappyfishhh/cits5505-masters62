6a053ce3afd14bec9c5223d527edc056a881e7bf
chamodhii <wgrangika@gmail.com>
2025-05-02 17:09:22 +0800

    Clean up old templates and static files; update base.html

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    // Wait for the DOM to be fully loaded
-
-    // Automatically hide flash messages after 3 seconds
-    setTimeout(function(){
-      $('.flashes').fadeOut(); // Smoothly fade out elements with class 'flashes'
-    }, 3000); // 3000 milliseconds = 3 seconds
-  });
-  
